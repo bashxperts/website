d85ff8c1e248ea3a805bf9690cf30c11a5b120a5
System Administrator <root@Viveks-MacBook-Air.local>
2023-02-18 21:29:05 +0530

    Spring App Added

---
+++ src/main/java/webapp/bashxperts/BashxpertsApplication.java
@@ -0,0 +1,13 @@
+package webapp.bashxperts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BashxpertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BashxpertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webapp/bashxperts/BashxpertsApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.bashxperts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BashxpertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
